e227bbb5d621ed29b2b4fbfc80ecd109f77addc7
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-07-26 00:21:55 +0300

    --update--

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,12 +24,11 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>Elewa Community</Typography>
+        <Typography color={medium}>elewasupport.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Need a place to build you career,just kick start with elewa.
       </Typography>
     </WidgetWrapper>
   );
